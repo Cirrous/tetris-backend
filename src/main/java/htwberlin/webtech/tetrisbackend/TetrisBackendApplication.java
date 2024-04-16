e7f90ab3c2b3f37315e736945875de7c106f9690
Cirrous <christianraisch@gmail.com>
2024-04-16 14:01:03 +0200

    initial commit

---
+++ src/main/java/htwberlin/webtech/tetrisbackend/TetrisBackendApplication.java
@@ -0,0 +1,13 @@
+package htwberlin.webtech.tetrisbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetrisBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetrisBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwberlin/webtech/tetrisbackend/TetrisBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package htwberlin.webtech.tetrisbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TetrisBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
