891b6c68936cbfc59933ebc83d367351a35c1507
Cirrous <christianraisch@gmail.com>
2024-06-17 16:16:21 +0200

    Added h2 database for testing

--- src/main/java/htwberlin/webtech/tetrisbackend/controllers/TetrisScoreController.java
+++ src/main/java/htwberlin/webtech/tetrisbackend/controllers/TetrisScoreController.java
@@ -5,9 +5,6 @@ import htwberlin.webtech.tetrisbackend.models.TetrisScoreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 public class TetrisScoreController {
 
